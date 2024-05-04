86b8a2c097d45e8b47b342425bb62c512867fa61
Teresa Esteban <teresaestegraci@gmail.com>
2024-05-04 16:18:34 +0200

    rolesusuario

--- proyecto2/proyecto2/database/seeders/DatabaseSeeder.php
+++ proyecto2/proyecto2/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,9 +15,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+            // Crear roles
+            $role1 = Role::create(['name' => 'employee']);
+            $role2 = Role::create(['name' => 'user']);
+
+            // Crear usuarios y asignarles roles
+            $user1 = User::factory()->create([
+                'name' => 'Employee User',
+                'email' => 'employee@gmail.com',
+                'password' => bcrypt('employee123'),
+            ]);
+            $user1->assignRole($role1);
+
+            $user2 = User::factory()->create([
+                'name' => 'Normal User',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('user123'),
+            ]);
+            $user2->assignRole($role2);
+
+
     }
 }
