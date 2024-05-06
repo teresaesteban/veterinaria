6ea13c418cdbc2543cf83d2963b3c3148acf345d
Teresa Esteban <teresaestegraci@gmail.com>
2024-05-06 15:43:44 +0200

    añadirbootstraphome

--- proyecto2/proyecto2/resources/views/home.blade.php
+++ proyecto2/proyecto2/resources/views/home.blade.php
@@ -1,111 +1,132 @@
 <x-app-layout>
   <x-slot name="header">
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('HOME') }}
-        </h2>
-</x-slot>
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+      {{ __('HOME') }}
+    </h2>
+  </x-slot>
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <title>New Age - Start Bootstrap Theme</title>
+      <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      <!-- Bootstrap icons-->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+      <!-- Google fonts-->
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+      <!-- Core theme CSS (includes Bootstrap)-->
+      <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+      <!-- Navigation-->
 
-  <div class="py-12 bg-dark"> <!-- Mantuve el fondo oscuro -->
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-dark overflow-hidden shadow-lg sm:rounded-lg"> <!-- Mantuve el fondo oscuro -->
-        <div class="p-6 text-white dark:text-white"> <!-- Cambié el color del texto a blanco -->
-          <!-- Contenido de la página de inicio -->
-
-          <div class="text-center">
-            <h1 class="text-4xl font-bold mb-4">Bienvenido a la Clínica Veterinaria</h1>
-            <p class="text-lg mb-8">Cuidando de tus mascotas como si fueran de la familia</p>
-
-
-            <!-- Añadí un espacio para el carrusel de imágenes -->
-            <div id="carouselExampleIndicators" class="carousel slide mb-8" data-bs-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="/images/carrusel1.webp" class="d-block w-100" alt="Imagen 1">
+      <header class="masthead bg-dark">
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center">
+                <div class="col-lg-6">
+                    <div class="mb-7 mb-lg-0 text-center text-lg-start">
+                        <h1 class="display-1 lh-1 mb-3">Bienvenido a la Clínica Veterinaria</h1>
+                        <p class="lead fw-normal text-muted mb-5">Cuidando de tus mascotas como si fueran de la familia</p>
+                    </div>
                 </div>
-                <div class="carousel-item">
-                  <img src="/images/carrusel3.webp" class="d-block w-100" alt="Imagen 3">
+                <div class="col-lg-6">
+                    <div class="masthead-device-mockup position-relative">
+                        <!-- SVG del círculo de fondo -->
+                        <svg class="circle position-absolute" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                            <defs>
+                                <linearGradient id="circleGradient" gradientTransform="rotate(45)">
+                                    <stop class="gradient-start-color" offset="0%"></stop>
+                                    <stop class="gradient-end-color" offset="60%"></stop>
+                                </linearGradient>
+                            </defs>
+                            <circle cx="50" cy="50" r="50" fill="url(#circleGradient)"></circle>
+                        </svg>
+                        <!-- Imagen encima del círculo -->
+                        <img class="position-relative" src="images/image_processing20200423-26081-c7tp2o.png" alt=""width="60%">
+                    </div>
                 </div>
-                <div class="carousel-item">
-                  <img src="/images/carrusel4.webp" class="d-block w-100" alt="Imagen 4">
-                </div>
-                <!-- Añade más imágenes según necesites -->
-              </div>
-
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
             </div>
-            <!-- Cambié el color del botón a blanco -->
-            <a href="/servicios" class="bg-white hover:bg-gray-200 text-gray-900 font-bold py-3 px-6 rounded inline-block"> <!-- Cambié el color del botón a blanco -->
-                Nuestros Servicios
-            </a>
-          </div>
-
-          <div class="mt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md">
-              <h2 class="text-2xl font-bold mb-2 text-gray-700 dark:text-blue-400">Experiencia</h2>
-              <p class="text-gray-800 dark:text-gray-300">Contamos con más de 20 años de experiencia en el cuidado de mascotas.</p>
-          </div>
-
-
-            <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md"> <!-- Mantuve el color del fondo -->
-              <h2 class="text-2xl font-bold mb-2 text-gray-700 dark:text-white">Equipo Profesional</h2> <!-- Cambié el color del texto a blanco -->
-              <p class="text-gray-500 dark:text-gray-300">Nuestro equipo está compuesto por veterinarios altamente capacitados y amantes de los animales.</p> <!-- Cambié el color del texto a gris claro -->
+        </div>
+    </header>
+  <!-- Quote/testimonial aside-->
+  <aside class="text-center bg-gradient-primary-to-secondary">
+    <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-xl-8">
+              <div class="h2 fs-1 text-white mb-4">"Una forma sencilla de gestionar las visitas y cuidar de tus compañeros peludos, ¡todo en una sola aplicación!"</div>
             </div>
-            <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md"> <!-- Mantuve el color del fondo -->
-              <h2 class="text-2xl font-bold mb-2 text-gray-700 dark:text-white">Atención Personalizada</h2> <!-- Cambié el color del texto a blanco -->
-              <p class="text-gray-500 dark:text-gray-300">Nos preocupamos por cada mascota como si fuera nuestra, ofreciendo un trato personalizado y dedicado.</p> <!-- Cambié el color del texto a gris claro -->
-            </div>
-          </div>
+        </div>
+    </div>
+</aside>
 
-          <div class="mt-12">
-            <h2 class="text-2xl font-bold mb-4">Nuestros Servicios</h2>
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md"> <!-- Mantuve el color del fondo -->
-                <h3 class="text-xl font-bold mb-2 text-gray-700 dark:text-white">Consultas Médicas</h3> <!-- Cambié el color del texto a blanco -->
-                <p class="text-gray-500 dark:text-gray-300">Realizamos consultas médicas completas para diagnosticar y tratar cualquier problema de salud que pueda tener tu mascota.</p> <!-- Cambié el color del texto a gris claro -->
-              </div>
-              <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md"> <!-- Mantuve el color del fondo -->
-                <h3 class="text-xl font-bold mb-2 text-gray-700 dark:text-white">Vacunaciones</h3> <!-- Cambié el color del texto a blanco -->
-                <p class="text-gray-500 dark:text-gray-300">Ofrecemos un completo programa de vacunación para prevenir enfermedades comunes en mascotas.</p> <!-- Cambié el color del texto a gris claro -->
-              </div>
-              <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md"> <!-- Mantuve el color del fondo -->
-                <h3 class="text-xl font-bold mb-2 text-gray-700 dark:text-white">Cirugías</h3> <!-- Cambié el color del texto a blanco -->
-                <p class="text-gray-500 dark:text-gray-300">Realizamos cirugías de rutina y especializadas con equipos modernos y bajo la supervisión de nuestros expertos veterinarios.</p> <!-- Cambié el color del texto a gris claro -->
-              </div>
-              <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md"> <!-- Mantuve el color del fondo -->
-                <h3 class="text-xl font-bold mb-2 text-gray-700 dark:text-white">Odontología</h3> <!-- Cambié el color del texto a blanco -->
-                <p class="text-gray-500 dark:text-gray-300">Cuidamos la salud dental de tu mascota con limpiezas, extracciones y tratamientos periodontales.</p> <!-- Cambié el color del texto a gris claro -->
-              </div>
-              <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md"> <!-- Mantuve el color del fondo -->
-                <h3 class="text-xl font-bold mb-2 text-gray-700 dark:text-white">Exámenes de Laboratorio</h3> <!-- Cambié el color del texto a blanco -->
-                <p class="text-gray-500 dark:text-gray-300">Contamos con laboratorio propio para realizar análisis clínicos, pruebas de diagnóstico y seguimiento de tratamientos.</p> <!-- Cambié el color del texto a gris claro -->
-              </div>
-              <div class="bg-gray-100 dark:bg-gray-700 p-6 rounded-lg shadow-md"> <!-- Mantuve el color del fondo -->
-                <h3 class="text-xl font-bold mb-2 text-gray-700 dark:text-white">... y mucho más</h3> <!-- Cambié el color del texto a blanco -->
-                <p class="text-gray-500 dark:text-gray-300">Además de los servicios mencionados, ofrecemos una amplia gama de servicios adicionales para el bienestar integral de tu mascota.</p> <!-- Cambié el color del texto a gris claro -->
-              </div>
-            </div>
-          </div>
-
-          <div class="mt-12">
-            <h2 class="text-2xl font-bold mb-4">Contáctanos</h2>
-            <p class="text-lg mb-4 text-white dark:text-white">¿Listo para agendar una cita? ¡Consigue la tuya ahora mismo!</p> <!-- Cambié el color del texto a blanco -->
-            <!-- Cambié el color del botón a blanco -->
-            <a href="/calendario" class="bg-white hover:bg-gray-200 text-gray-900 font-bold py-3 px-6 rounded inline-block"> <!-- Cambié el color del botón a blanco -->
-                Consigue cita
-            </a>
-          </div>
-          <!-- Fin del contenido de la página de inicio -->
+     <!-- App features section-->
+<section id="features">
+  <div class="container px-5">
+    <div class="row gx-5 justify-content-center">
+      <div class="col-md-6 mb-5">
+        <!-- Feature item: Calendario -->
+        <div class="text-center">
+          <i class="bi-calendar icon-feature text-gradient d-block mb-3"></i>
+          <h3 class="font-alt">Calendario</h3>
+          <p class="text-muted mb-0">Organiza citas y recordatorios importantes para tu mascota.</p>
+        </div>
+      </div>
+      <div class="col-md-6 mb-5">
+        <!-- Feature item: Historial Clínico -->
+        <div class="text-center">
+          <i class="bi-file-earmark-medical icon-feature text-gradient d-block mb-3"></i>
+          <h3 class="font-alt">Historial Clínico</h3>
+          <p class="text-muted mb-0">Mantén un registro detallado de las visitas al veterinario, tratamientos y más.</p>
+        </div>
+      </div>
+      <div class="col-md-6 mb-5">
+        <!-- Feature item: Free to Use -->
+        <div class="text-center">
+          <i class="bi bi-heart icon-feature text-gradient d-block mb-3"></i>
+          <h3 class="font-alt">¿Qué le pasa a tu Mascota?</h3>
+          <p class="text-muted mb-0">Pregunta a nuestros especialistas cualquier duda que te pueda surgir en el cuidado de tu mascota</p>
+        </div>
+      </div>
+      <div class="col-md-6 mb-5">
+        <!-- Feature item: ¿Qué le pasa a tu Mascota? -->
+        <div class="text-center">
+          <i class="bi bi-house-door icon-feature text-gradient d-block mb-3"></i>
+          <h3 class="font-alt">Sobre Nosotros</h3>
+          <p class="text-muted mb-0">En la página principal podrás encontrar la información general de su funcionamiento</p>
         </div>
       </div>
     </div>
   </div>
+</section>
+        <!-- Basic features section-->
+
+        <section class="bg-gradient-primary-to-secondary">
+          <div class="container px-5">
+            <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+              <div class="col-sm-8 col-md-6">
+                <div class="px-5 px-sm-0">
+                  <img class="img-fluid" src="images/vet-assistant-png-dog-lady-people-pets-veterinarian-clipart-1212.png" alt="Imagen Sobre Nosotros" />
+                </div>
+              </div>
+              <div class="col-12 col-lg-5">
+                <h2 class="display-4 lh-1 mb-4">Sobre Nosotros</h2>
+                <p class="text-white mb-5">
+                  En VetClinic, nos esforzamos por brindar un entorno amigable y eficiente para que los veterinarios puedan registrar y acceder fácilmente a la información de los pacientes, administrar citas, llevar un historial clínico completo, y proporcionar un seguimiento más preciso y personalizado del estado de salud de las mascotas.
+                </p>
+                <p class="text-white mb-0"> <!-- Cambio de color del texto a primario -->
+                  Con VetClinic, esperamos contribuir al bienestar y cuidado de todas las mascotas, ofreciendo a los profesionales veterinarios las herramientas necesarias para un servicio de calidad y a los dueños de mascotas la tranquilidad de contar con un equipo comprometido con la salud y felicidad de sus compañeros peludos.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
 </x-app-layout>
 @include('registro.footer')
