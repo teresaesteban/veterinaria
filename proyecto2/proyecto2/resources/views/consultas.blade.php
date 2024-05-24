44e69312aec35fe3f29467cecbdb881da7721313
Teresa Esteban <teresaestegraci@gmail.com>
2024-05-24 20:58:18 +0200

    color boton eliminar

--- proyecto2/proyecto2/resources/views/consultas.blade.php
+++ proyecto2/proyecto2/resources/views/consultas.blade.php
@@ -54,10 +54,8 @@
                         <form action="{{ route('consultas.destroy', $consulta->id) }}" method="POST" class="text-right">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="text-red-500 hover:text-red-700 focus:outline-none" id="delete-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
+                            <button type="submit" class="btn btn-danger" id="delete-btn">
+                                Eliminar
                             </button>
                         </form>
                         @endif
